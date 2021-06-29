ab8ab5696365e700213045312435927f096ada66
Abipandi <85838719+Abipandi@users.noreply.github.com>
2021-06-30 00:23:05 +0530

    Add files via upload

---
+++ day9out.py
@@ -0,0 +1,55 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/smile/AppData/Local/Programs/Python/Python39/day9.py ====
+2
+4
+6
+8
+10
+12
+14
+54321
+5432
+543
+54
+5
+Enter the value of 'n': 5
+Fibonacci Series:  0 1 1 2 3 0
+1
+153
+370
+371
+407
+The muliplication table of 9 :
+9 x 1 = 9
+9 x 2 = 18
+9 x 3 = 27
+9 x 4 = 36
+9 x 5 = 45
+9 x 6 = 54
+9 x 7 = 63
+9 x 8 = 72
+9 x 9 = 81
+9 x 10 = 90
+Enter a number: 4
+Positive number
+Enter the days:
+2
+0  Year,  0  Weeks, and  2  Days
+
+Enter the number:7
+sin(x)= 0.6569865987187891
+cos(x)= 0.7539022543433046
+tan(x)= 0.8714479827243188
+(sin(x)+cos(x))/tan(x)= 1.6190167181882458
+Calculator
+1.Add
+2.Substract
+3.Multiply
+4.Divide
+Enter the Choice(1-4): 3
+Enter A:2
+Enter B:1
+Product =  2
+>>> 
